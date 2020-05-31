dbf5d777f048f9096cafbf97714e9963b3af918f
weilei <1149327214@qq.com>
2020-05-31 15:25:36 +0800

    feature():Python Crash Course lesson2 1.Description: as title

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
 message="hello git world!"
 print(message)
+
+message="Hello Python crash course world"
+
+print(message)
+
+simple="This is second message"
+print(simple)
+
+simple="This test is end"
+print(simple);
---
+++ name.py
@@ -0,0 +1,53 @@
+name="ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+test=" hello "
+print(test.lstrip());
+print(test.rstrip());
+print(test.strip());
+
+name="Eric"
+show="Hello"+name+","+"would you like to learn some Python today?"
+print(show)
+
+name="wei lei"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+
+phrase='Albert Einstein once said,"A person who nerver made a mistake never tried anything new"'
+print(phrase)
+
+
+famous_person="Albert Einstein"
+message=famous_person+" once said,"+'"A person who nerver make a mistake nerver tried anything new."'
+print(message)
+
+name="\t wei lei \t\n"
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+age=23
+message="Happy" + str(age)+"rd Birthday"
+print(message)
+
+
+
+num=123
+message="my farvarout number is"+" "+str(num)+"!\n"
+print(message)
+
+
+
+
+
+
+
+
+
+
