6c6b5209dc85b50f44f66bd2772bc126cdc8b8d5
weilei <1149327214@qq.com>
2020-06-06 18:38:33 +0800

    feature:1.add lesson4 code 1.Description:as title

---
+++ Lesson_4.py
@@ -0,0 +1,106 @@
+# magicians=['alice', 'david', 'carolina']
+# for magician in magicians:
+# 	print(magician.title()+", that was a great trick!")
+# 	print("I can not wait to see your next trick,"+ magician.title()+"\n")
+# print("thank you,everyone.That was a great magic show!")
+#
+# Citys = ["BeiJing", "ChengDu", "MianYang"]
+# for City in Citys:
+# 	print(City)
+# 	print("I like city,"+City)
+# print("my favorite like city is "+Citys[1])
+#
+# Dogs = ['dog', 'cat', 'panda']
+# for dog in Dogs:
+# 	print("wow! "+dog)
+# print("ccc,"+Dogs[0])
+# for value in range(1,5):
+# 	print(value)
+# numbers=list(range(1,5))
+# print(numbers)
+# squares=[]
+# for value in range(1,11):
+# 	square=value**2
+# 	squares.append(square)
+# print(squares)
+# print(min(squares))
+# print(max(squares))
+# print(sum(squares))
+#
+# squares=[value**2 for value in range(1,5)]
+# print(squares)
+#
+# for value in range(1,21):
+# 	print(value)
+
+# numbers=list(range(1,1000000))
+# for number in numbers:
+# 	print(number)
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# numbers=list(range(1,20,2))
+# for number in numbers:
+# 	print(number)
+# print(numbers)
+# for number in range(1,20,2):
+# 	print(number)
+# numbers=list(range(3,30,3))
+# for number in numbers:
+# 	print(number)
+# numbers=[value**3 for value in range(1,11)]
+# for number in numbers:
+# 	print(number)
+# numbers=[]
+# for number in range(1,11):
+# 	numbers.append(number**3)
+# print(numbers)
+
+#players=['how','charles','eli']
+#print(players[0:2])
+
+
+# my_foods=['pizza','falafel','carrot cake']
+# friend_foods=my_foods[:]
+# print("my:")
+# print(friend_foods)
+
+# Citys=['BeiJing','ChengDu','XiAn','HangZhou']
+# print("The first three items in the list are:")
+# print(Citys[:3])
+# print("Three items from the middle of the list are:")
+# print(Citys[:])
+# print("Last three:")
+# print(Citys[-3:])
+
+# dimensions=(200,50)
+# print("original dimensions:")
+# for dimension in dimensions:
+# 	print(dimension)
+# dimensions=(400,40)
+# print("new dimensions:")
+# for dimension in dimensions:
+# 	print(dimension)
+
+foods=('mifan','mantou','rou')
+for food in foods:
+	print(food)
+foods=('x',100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
